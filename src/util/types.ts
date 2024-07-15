7a83b0fdb85aceb79d140e15aff61f97b15b0826
Justin Zint <zintjj@gmail.com>
2024-07-15 18:11:43 -0500

    * Add Array & Object path types

--- src/util/types.ts
+++ src/util/types.ts
@@ -1,1 +1,16 @@
-export { Paths, Get, JsonValue as Json, OmitDeep } from 'type-fest';
+export {
+  Paths,
+  Get,
+  JsonValue as Json,
+  OmitDeep,
+  ArrayValues,
+} from 'type-fest';
+import { Paths, Get, JsonValue as Json } from 'type-fest';
+
+export type ArrayPaths<T> = {
+  [K in Paths<T>]: Get<T, K> extends Json[] ? K : never;
+}[Paths<T>];
+
+export type ObjectPaths<T> = {
+  [K in Paths<T>]: Get<T, K> extends Record<string, Json> ? K : never;
+}[Paths<T>];
